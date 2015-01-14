e902d21ee4ee22a3226cfef231eab1fb6501b2d7
Andy Cochran <acochran@openplans.org>
2015-01-14 17:59:14 -0500

    toggle and scroll to project list

--- _site/main.js
+++ _site/main.js
@@ -2,3 +2,13 @@
 
 $(document).foundation();
 
+$('#project-list').hide();
+$('.project-list-button').click(function(e) {
+  e.preventDefault();
+  if ($('#project-list:hidden').length) {
+    $('#project-list').slideDown('fast');
+    $('html, body').animate({ scrollTop: $(this).offset().top - 30 }, 200);
+  } else {
+    $('html, body').animate({ scrollTop: $(this).offset().top - 30 }, 200);
+  }
+});
