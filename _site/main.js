514132dbccd0f5fbbc8e77de75464dc50eb36f66
Andy Cochran <acochran@openplans.org>
2015-01-28 13:18:55 -0500

    note in js: need to recalculate position of topbar when project list open

--- _site/main.js
+++ _site/main.js
@@ -7,8 +7,16 @@ $('.project-list-button').click(function(e) {
   e.preventDefault();
   if ($('#project-list:hidden').length) {
     $('#project-list').slideDown('fast');
-    $('html, body').animate({ scrollTop: $(this).offset().top - 30 }, 200);
+    $('html, body').animate({ scrollTop: $(this).offset().top - 30 }, 200, function(){
+      // need to recalculate position of topbar when project list open
+      // $(document).foundation('magellan', 'reflow');
+      // ^ this doesn't work
+    });
   } else {
-    $('html, body').animate({ scrollTop: $(this).offset().top - 30 }, 200);
+    $('html, body').animate({ scrollTop: $(this).offset().top - 30 }, 200, function(){
+      // need to recalculate position of topbar when project list open
+      // $(document).foundation('magellan', 'reflow');
+      // ^ this doesn't work
+    });
   }
 });
